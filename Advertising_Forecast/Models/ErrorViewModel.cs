f54a2b1c318c01313b8a01edea92ea50b168d890
mbgisler <projeisim@gmail.com>
2024-07-15 22:29:43 +0300

    Add project files.

---
+++ Advertising_Forecast/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Advertising_Forecast.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
